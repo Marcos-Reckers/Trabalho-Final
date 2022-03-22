0ca07c87065ac9e43862a7226b098bbec861f6d0
mifax <86305120+mifax@users.noreply.github.com>
2022-03-21 23:42:25 -0300

    .

--- Codigo/BatleINF.c
+++ Codigo/BatleINF.c
@@ -8,27 +8,47 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 600;
     float velocidade = 3.0;
+    int rotation = 0;
     InitWindow(screenWidth, screenHeight, "BattleINF");
 
 
-    // PLAYER VARIABELS
+    // PLAYER VARIABELS =====================================================================
 
     // Textures (for getting width and height)
     Texture2D player = LoadTexture("Assets/player_u.png");  
 
-    int frameWidth = (player.width);
-    int frameHeight = (player.height);
+    int player_frameWidth = (player.width);
+    int player_frameHeight = (player.height);
 
     // Source rectangle (part of the texture to use for drawing)
-    Rectangle sourceRec = { 0, 0, (float)frameWidth, (float)frameHeight };
+    Rectangle player_sourceRec = { 0, 0, (float)player_frameWidth, (float)player_frameHeight };
 
     // Destination rectangle (screen rectangle where drawing part of texture)
-    Rectangle destRec = { screenWidth/2.0f, screenHeight/2.0f, 60,60};
+    Rectangle player_destRec = { screenWidth/2.0f, screenHeight/2.0f, 60,60};
 
     // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
-    Vector2 origin = { 0,0 };
+    Vector2 player_origin = { 0,0 };
+    //======================================================================================
 
-    int rotation = 0;
+
+
+    // ENEMY VARIABELS =====================================================================
+
+    // Textures (for getting width and height)
+    Texture2D enemy = LoadTexture("Assets/enemy_u.png");  
+
+    int enemy_frameWidth = (enemy.width);
+    int enemy_frameHeight = (enemy.height);
+
+    // Source rectangle (part of the texture to use for drawing)
+    Rectangle enemy_sourceRec = { 0, 0, (float)enemy_frameWidth, (float)enemy_frameHeight };
+
+    // Destination rectangle (screen rectangle where drawing part of texture)
+    Rectangle enemy_destRec = { screenWidth/2.0f, screenHeight/2.0f, 60,60};
+
+    // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
+    Vector2 enemy_origin = { 0,0 };
+    //======================================================================================
     
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -39,32 +59,66 @@ int main()
         // Update
         //----------------------------------------------------------------------------------
 
-        // MOVE
+        // MOVE PLAYER =====================================================================
         if (IsKeyDown(KEY_RIGHT))
         {
-            destRec.x += velocidade;
+            player_destRec.x += velocidade;
             player = LoadTexture("Assets/player_r.png");
             // rotation = 90;
         }
         else if (IsKeyDown(KEY_LEFT))
         {
-            destRec.x -= velocidade;
+            player_destRec.x -= velocidade;
             player = LoadTexture("Assets/player_l.png");
             // rotation = 270;
         }
         else if (IsKeyDown(KEY_UP))
         {
-            destRec.y -= velocidade;
+            player_destRec.y -= velocidade;
             player = LoadTexture("Assets/player_u.png");
             // rotation = 0;
         }
         else if (IsKeyDown(KEY_DOWN))
         {
-            destRec.y += velocidade;
+            player_destRec.y += velocidade;
             player = LoadTexture("Assets/player_d.png");
             // rotation = 180;
         }
-        //----------------------------------------------------------------------------------
+        //==================================================================================
+
+
+
+        // MOVE ENEMY =====================================================================
+        if (IsKeyDown(KEY_D))
+        {
+            enemy_destRec.x += velocidade;
+            enemy = LoadTexture("Assets/enemy_r.png");
+            // rotation = 90;
+        }
+        else if (IsKeyDown(KEY_A ))
+        {
+            enemy_destRec.x -= velocidade;
+            enemy = LoadTexture("Assets/enemy_l.png");
+            // rotation = 270;
+        }
+        else if (IsKeyDown(KEY_W))
+        {
+            enemy_destRec.y -= velocidade;
+            enemy = LoadTexture("Assets/enemy_u.png");
+            // rotation = 0;
+        }
+        else if (IsKeyDown(KEY_S))
+        {
+            enemy_destRec.y += velocidade;
+            enemy = LoadTexture("Assets/enemy_d.png");
+            // rotation = 180;
+        }
+        //==================================================================================
+
+
+
+        
+
 
         //----------------------------------------------------------------------------------
 
@@ -73,8 +127,11 @@ int main()
         BeginDrawing();
 
         ClearBackground(BLACK);
-        
-        DrawTexturePro(player, sourceRec, destRec, origin, (float)rotation, WHITE);
+        // Draw Player
+        DrawTexturePro(player, player_sourceRec, player_destRec, player_origin, (float)rotation, WHITE);
+
+        // Draw Enemy
+        DrawTexturePro(enemy, enemy_sourceRec, enemy_destRec, enemy_origin, (float)rotation, WHITE);
 
         
 
@@ -85,6 +142,8 @@ int main()
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadTexture(player); 
+    UnloadTexture(enemy); 
+    
     CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
